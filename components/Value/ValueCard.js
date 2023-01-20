be82a4ab14deeb98d45a9fb5655f9640deb72e8e
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-01-20 15:16:21 +0800

    fix: values for loyalty and respect styling

--- components/Value/ValueCard.js
+++ components/Value/ValueCard.js
@@ -7,19 +7,51 @@ export default function ValueCard(props) {
         <div className="w-full lg:w-3/6 my-auto">
             <Image src={image} alt="Value" className="invisible" width={10} height={10}/>
         </div>
-        <div className="absolute flex -top-4 left-0 lg:left-0 w-full h-fit p-6 border-2 border-box-inno-outer bg-box-inno">
-            <div className="flex flex-col w-full mb-3 text-white text-center justify-center">
-                <div>
-                    <div className="image-column">
-                        <div className="image-container">
-                            <Image src={image} alt="Value" width={15} height={15} className=""/>
+        {nama == 'INNOVATIVE' && (
+            <div className="absolute flex -top-4 left-0 lg:left-0 w-full h-fit p-6 border-2 border-box-inno-outer bg-box-inno">
+                <div className="flex flex-col w-full mb-3 text-white text-center justify-center">
+                    <div>
+                        <div className="image-column">
+                            <div className="image-container">
+                                <Image src={image} alt="Value" width={15} height={20} className=""/>
+                            </div>
                         </div>
+                        <h4 className="font-bold text-lg mt-2">{nama}</h4>
+                        <p className="font-normal text-sm mt-4">{deskripsi}</p>
                     </div>
-                    <h4 className="font-bold text-lg">{nama}</h4>
-                    <p className="font-normal text-sm mt-4">{deskripsi}</p>
                 </div>
             </div>
-        </div>
+        )}
+        {nama == 'LOYALTY' && (
+            <div className="absolute flex -top-4 left-0 lg:left-0 w-full h-fit p-6 border-2 border-box-loyalty-outer bg-box-loyalty">
+                <div className="flex flex-col w-full mb-3 text-white text-center justify-center">
+                    <div>
+                        <div className="image-column">
+                            <div className="image-container">
+                                <Image src={image} alt="Value" width={25} height={15} className=""/>
+                            </div>
+                        </div>
+                        <h4 className="font-bold text-lg mt-2">{nama}</h4>
+                        <p className="font-normal text-sm mt-4">{deskripsi}</p>
+                    </div>
+                </div>
+            </div>
+        )}
+        {nama == 'RESPECT' && (
+            <div className="absolute flex -top-4 left-0 lg:left-0 w-full h-fit p-6 border-2 border-box-respect-outer bg-box-respect">
+                <div className="flex flex-col w-full mb-3 text-white text-center justify-center">
+                    <div>
+                        <div className="image-column">
+                            <div className="image-container">
+                                <Image src={image} alt="Value" width={30} height={15} className=""/>
+                            </div>
+                        </div>
+                        <h4 className="font-bold text-lg mt-2">{nama}</h4>
+                        <p className="font-normal text-sm mt-4">{deskripsi}</p>
+                    </div>
+                </div>
+            </div>
+        )}
     </div>
   );
 }
