abc2d55c02a3a21aefaa73b5245f83ea141b2aeb
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-01-20 10:03:17 +0800

    fix: revise footer links

--- components/Footer.js
+++ components/Footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
             <div className='text-center py-4 bg-footer'>
                 <p className='text-white mt-4'>Copyright @ 2016. PT Company</p>
                 <div className='w-fit mx-auto mb-3 mt-2'>
-                    <a href="https://www.facebook.com/baksorusukjoss/">
+                    <a href="https://www.facebook.com/">
                         <Image src='/img/facebook-official.png' alt='Facebook' width={30} height={30} className='inline scale-[70%] mx-2 hover:scale-75 duration-100' />
                     </a>
-                    <a href="https://www.twitter.com/BaksoRusukJossPusat/">
+                    <a href="https://www.twitter.com/">
                         <Image src='/img/twitter.png' alt='Twitter' width={30} height={30} className='inline scale-[70%] mx-2 hover:scale-75 duration-100' />
                     </a>
                 </div>
