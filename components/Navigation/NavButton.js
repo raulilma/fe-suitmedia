5d981bc5649e9975091f52cce19f29a7eb10f780
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-01-20 13:27:19 +0800

    fix: styling and effects on navbar

--- components/Navigation/NavButton.js
+++ components/Navigation/NavButton.js
@@ -1,7 +1,7 @@
 export default function NavButton(props){
   return(
         <a href={props.href}
-            className="m-4 hover:scale-105 duration-100">
+            className="m-4 hover:scale-105 hover:bg-softgray duration-100 pt-2 pb-2">
             <p className="text-sm px-3 py-1">{props.title}</p>
         </a>
   );
